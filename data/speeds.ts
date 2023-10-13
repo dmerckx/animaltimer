88c6fd0e4991b777df755dfa6c15c9bdd547a674
David Merckx <david@codeurs.be>
2023-10-13 14:44:26 +0200

    Init

---
+++ data/speeds.ts
@@ -0,0 +1,87 @@
+export const speeds = [
+  {
+    name: "Snail",
+    speed: 0.048,
+    emoji: "🐌",
+  },
+  {
+    name: "Tortoise",
+    speed: 0.32,
+    emoji: "🐢",
+  },
+  {
+    name: "Penguin",
+    speed: 2.4,
+    emoji: "🐧",
+  },
+  {
+    name: "Spider",
+    speed: 1.88,
+    emoji: "🕷️",
+  },
+  {
+    name: "Goldfish",
+    speed: 3.2,
+    emoji: "🐟",
+  },
+  {
+    name: "Hamster",
+    speed: 4.8,
+    emoji: "🐹",
+  },
+  {
+    name: "Raccoon",
+    speed: 6,
+    emoji: "🦝",
+  },
+  {
+    name: "Hedgehog",
+    speed: 6.4,
+    emoji: "🦔",
+  },
+  {
+    name: "Frog",
+    speed: 8,
+    emoji: "🐸",
+  },
+  {
+    name: "Duck",
+    speed: 10.5,
+    emoji: "🦆",
+  },
+  {
+    name: "House mouse",
+    speed: 12.9,
+    emoji: "🐁",
+  },
+  {
+    name: "Chicken",
+    speed: 14.5,
+    emoji: "🐔",
+  },
+  {
+    name: "Pig",
+    speed: 17.7,
+    emoji: "🐖",
+  },
+  {
+    name: "Squirrel",
+    speed: 19.3,
+    emoji: "🐿️",
+  },
+  {
+    name: "Goat",
+    speed: 24.1,
+    emoji: "🐐",
+  },
+  {
+    name: "Sheep",
+    speed: 32.2,
+    emoji: "🐏",
+  },
+  {
+    name: "Domestic cat",
+    speed: 48.3,
+    emoji: "🐈",
+  },
+];
